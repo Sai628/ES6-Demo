cac8baf1d6dc738affa4023ba494093f638bdef3
Sai628 <sai100628@gmail.com>
2016-12-16 17:53:31 +0800

    add destructuing

---
+++ src/04_destructuring.js
@@ -0,0 +1,11 @@
+
+// Desctructuring Arrays
+let [a, b, c, d] = [1, 2, 3, 4]
+console.log(a)  // 1
+console.log(c)  // 3
+
+
+// Desctructuring Objects
+let {occupation, father} = {occupation: 'jedi', father: 'anakin'}
+console.log(occupation)  // jedi
+console.log(father)  // anakin
