f7e467fa5df5dfc5633e317114572d621c99dcf1
Sai628 <sai100628@gmail.com>
2016-12-16 16:07:31 +0800

    add strings

---
+++ src/02_strings.js
@@ -0,0 +1,10 @@
+
+// .includes()
+let string = 'food'
+let substring = 'foo'
+console.log(string.includes(substring))  // true
+
+
+// .repeat()
+let meow = 'meow'.repeat(3)
+console.log(meow)  // meowmeowmeow
