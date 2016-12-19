ac04ddeb15fa59f4b6be4928bb504e206b5bde5f
Sai628 <sai100628@gmail.com>
2016-12-19 11:45:46 +0800

    update "Template literals" demo

--- src/03_template_literals.js
+++ src/03_template_literals.js
@@ -8,14 +8,42 @@ let age = 13
 console.log(`My cat is named ${name} and is ${age} years old.`)  // My cat is named Tiger and is 13 years old.
 
 
-let lines = `cat
+let lines = (`cat
 dog
-nickelodeon`
+nickelodeon`)
 console.log(lines)
-// cat
-// dog
-// nickelodeon
+/*
+cat
+dog
+nickelodeon
+*/
 
 
+// Template literals can accept expressions
 let today = new Date()
 console.log(`The time and date is ${today.toLocaleString()}`)  // The time and date is 12/16/2016, 4:18:48 PM
+
+
+let book = {
+    title: 'Harry Potter and The Sorcercers Stone',
+    summary: 'Much magic. Such depth.',
+    author: 'J.K. Rowling'
+}
+
+let html = `<header>
+<h1>${book.title}</h1>
+</header>
+<section>
+    <div>${book.summary}</div>
+    <div>${book.author}</div>
+</section>`
+console.log(html)
+/*
+<header>
+<h1>Harry Potter and The Sorcercers Stone</h1>
+</header>
+<section>
+    <div>Much magic. Such depth.</div>
+    <div>J.K. Rowling</div>
+</section>
+*/
