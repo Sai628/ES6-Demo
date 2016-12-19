0f78adbe4baadf8066df7fd565da417d308d9725
Sai628 <sai100628@gmail.com>
2016-12-19 14:04:12 +0800

    update "desctructuring"

--- src/04_destructuring.js
+++ src/04_destructuring.js
@@ -9,3 +9,15 @@ console.log(c)  // 3
 let {occupation, father} = {occupation: 'jedi', father: 'anakin'}
 console.log(occupation)  // jedi
 console.log(father)  // anakin
+
+
+function getCoords() {
+    return {
+        x: 10,
+        y: 22
+    }
+}
+
+let {x, y} = getCoords()
+console.log(x)  // 10
+console.log(y)  // 22
